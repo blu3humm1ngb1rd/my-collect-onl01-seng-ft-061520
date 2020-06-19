2de24eb025f5869e309d38592e5aa9f384900ea4
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-19 02:45:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,3 @@
 
-def my_collect(array)
-  if block_given?
-   counter = 0 
-   while counter < array.length 
-    yield(array[counter])
-   counter += 1 
- end
- else
-   "Need data"
- end 
-end
 
 
