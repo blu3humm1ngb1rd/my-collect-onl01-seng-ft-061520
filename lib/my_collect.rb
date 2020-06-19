6b032256c43265cf24767c4e7721dfa2ef99727a
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-19 02:52:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,8 @@
 def my_collect(array)
   counter = 0 
+  return_array = []
   while counter < array.length 
-  return_array[] = yield(array[counter])
+  return_array = yield(array[counter])
   counter += 1 
 end 
   return_array
